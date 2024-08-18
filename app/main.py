2775d4d163cd4d2d85dca10300d09e17293670d3
Nairaceorex <63120787+Nairaceorex@users.noreply.github.com>
2024-08-19 00:40:48 +0300

    create post-request tasks

--- app/main.py
+++ app/main.py
@@ -1,5 +1,20 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+
 from database import Base, engine, get_db
 from models import Task
 from schemas import TaskCreate, Task
 
 Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+@app.post("/tasks/", response_model=Task)
+def create_task(task: TaskCreate, db: Session = Depends(get_db)):
+    db_task = Task(title=task.title, description=task.description, status=task.status)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
