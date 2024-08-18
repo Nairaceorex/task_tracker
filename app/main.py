f003aa45bb5f69714cee5a93d2a759ed15386d57
Nairaceorex <63120787+Nairaceorex@users.noreply.github.com>
2024-08-19 00:41:55 +0300

    create put-request task by id

--- app/main.py
+++ app/main.py
@@ -24,3 +24,24 @@ def create_task(task: TaskCreate, db: Session = Depends(get_db)):
 def read_tasks(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     tasks = db.query(Task).offset(skip).limit(limit).all()
     return tasks
+
+
+@app.get("/tasks/{task_id}", response_model=Task)
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.id == task_id).first()
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, task: TaskCreate, db: Session = Depends(get_db)):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    db_task.title = task.title
+    db_task.description = task.description
+    db_task.status = task.status
+    db.commit()
+    db.refresh(db_task)
+    return db_task
