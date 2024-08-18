5a3f37588d982dc59812d783b2dc024ecc3be59f
Nairaceorex <63120787+Nairaceorex@users.noreply.github.com>
2024-08-19 01:02:22 +0300

    fixed error import module

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from database import Base, engine, get_db
-from models import Task
+from models import Task as TaskModel
+
 from schemas import TaskCreate, Task
 
 Base.metadata.create_all(bind=engine)
@@ -13,11 +14,12 @@ app = FastAPI()
 
 @app.post("/tasks/", response_model=Task)
 def create_task(task: TaskCreate, db: Session = Depends(get_db)):
-    db_task = Task(title=task.title, description=task.description, status=task.status)
+    db_task = TaskModel(title=task.title, description=task.description, status=task.status)
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
     return db_task
+
 
 
 @app.get("/tasks/", response_model=List[Task])
